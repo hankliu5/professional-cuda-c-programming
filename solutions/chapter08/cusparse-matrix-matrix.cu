97ea84affd7c40337f2d5748d3175f23d51e8148
hankliu5 <yliu719@ucr.edu>
2019-11-25 15:40:32 -0800

    implement dense2csr and densemm but results are different

--- solutions/chapter08/cusparse-matrix-matrix.cu
+++ solutions/chapter08/cusparse-matrix-matrix.cu
@@ -11,12 +11,6 @@
  * sparse matrix-vector multiplication on randomly generated data.
  */
 
-/*
- * M = # of rows
- * N = # of columns
- */
-// int M = 16384;
-// int N = 16384;
 
 /*
  * Generate random dense matrix A in column-major order, while rounding some
@@ -57,21 +51,52 @@ int generate_random_dense_matrix(int M, int N, float **outA)
     return totalNnz;
 }
 
-void print_partial_matrix(float *M, int nrows, int ncols, int max_row,
-        int max_col)
+void print_matrix(float *M, int nrows, int ncols)
 {
     int row, col;
 
-    for (row = 0; row < max_row; row++)
+    for (row = 0; row < nrows; row++)
     {
-        for (col = 0; col < max_col; col++)
+        for (col = 0; col < ncols; col++)
         {
             printf("%2.2f ", M[row * ncols + col]);
         }
-        printf("...\n");
+        printf("\n");
     }
-    printf("...\n");
 }
+
+
+void dense2csr(float *M, int nrows, int ncols, float *CsrVal, int *CsrRowPtr, int *CsrColInd) {
+    int i, j, num_of_NNZ = 0, count = 0;
+    CsrRowPtr[0] = 0;
+    for (i = 0; i < nrows; i++) {
+        for (j = 0; j < ncols; j++, count++) {
+            if (M[count] != 0.0f) {
+                CsrVal[num_of_NNZ] = M[count];
+                CsrColInd[num_of_NNZ] = j;
+                num_of_NNZ++;
+            }
+        }
+        CsrRowPtr[i+1] = num_of_NNZ;
+    }
+    printf("counts: %d\n", count);
+}
+
+void densemm(float *A, float *B, float *C, int nrows, int ncols) {
+    int i, j, k;
+    for(i = 0; i < nrows; i++)    
+    {    
+        for(j = 0; j < ncols; j++)    
+        {    
+            C[i * ncols + j] = 0;    
+            for(k = 0; k < ncols; k++)    
+            {    
+                C[i * ncols + j] += A[i * ncols + k] * B[k * ncols + j];    
+            }    
+        }    
+    }   
+}
+
 
 int main(int argc, char **argv)
 {
@@ -83,6 +108,19 @@ int main(int argc, char **argv)
     float *dCsrValA;
     int *dCsrRowPtrA;
     int *dCsrColIndA;
+
+    // transfer by CUDA
+    float *CsrValA_cuda;
+    int *CsrRowPtrA_cuda;
+    int *CsrColIndA_cuda;
+
+    // transfer by CPU
+    float *CsrValA_cpu;
+    int *CsrRowPtrA_cpu;
+    int *CsrColIndA_cpu;
+
+    int i;
+
     int totalANnz;
     float alpha = 3.0f;
     float beta = 4.0f;
@@ -108,9 +146,13 @@ int main(int argc, char **argv)
     printf("generate dataset: %ld msec\n", diff * 1000 / CLOCKS_PER_SEC);
 
     printf("A:\n");
-    print_partial_matrix(A, M, N, 10, 10);
+    print_matrix(A, M, N);
     printf("B:\n");
-    print_partial_matrix(B, N, M, 10, 10);
+    print_matrix(B, N, M);
+
+    densemm(A, B, C, M, N);
+    printf("C:\n");
+    print_matrix(C, M, N);
 
     // Create the cuSPARSE handle
     CHECK_CUSPARSE(cusparseCreate(&handle));
@@ -171,8 +213,39 @@ int main(int argc, char **argv)
                                       dCsrValA, dCsrRowPtrA, dCsrColIndA));
     diff = clock() - start;
     printf("cusparseSdense2csr: %ld msec\n", diff * 1000 / CLOCKS_PER_SEC);
-                                  
+    
+    CsrValA_cuda = (float *) malloc(sizeof(float) * totalANnz);
+    CsrRowPtrA_cuda = (int *) malloc(sizeof(int) * (M + 1));
+    CsrColIndA_cuda = (int *) malloc(sizeof(int) * totalANnz);
+    
+    CHECK(cudaMemcpy(CsrValA_cuda, dCsrValA, sizeof(float) * totalANnz, cudaMemcpyDeviceToHost));
+    CHECK(cudaMemcpy(CsrRowPtrA_cuda, dCsrRowPtrA, sizeof(int) * (M + 1), cudaMemcpyDeviceToHost));
+    CHECK(cudaMemcpy(CsrColIndA_cuda, dCsrColIndA, sizeof(int) * totalANnz, cudaMemcpyDeviceToHost));
 
+    CsrValA_cpu = (float *) malloc(sizeof(float) * totalANnz);
+    CsrRowPtrA_cpu = (int *) malloc(sizeof(int) * (M + 1));
+    CsrColIndA_cpu = (int *) malloc(sizeof(int) * totalANnz);
+
+    // Convert A from a dense formatting to a CSR formatting, using the GPU
+    printf("M: %d, N: %d\n", M, N);
+    start = clock();
+    dense2csr(A, M, N, CsrValA_cpu, CsrRowPtrA_cpu, CsrColIndA_cpu);
+    diff = clock() - start;
+    printf("dense2csr: %ld msec\n", diff * 1000 / CLOCKS_PER_SEC);
+
+    for (i = 0; i < totalANnz; i++) {
+        printf("%f %f\n", CsrValA_cuda[i], CsrValA_cpu[i]);
+    }
+
+    for (i = 0; i < (M + 1); i++) {
+        printf("%d %d\n", CsrRowPtrA_cuda[i], CsrRowPtrA_cpu[i]);
+    }
+
+    for (i = 0; i < totalANnz; i++) {
+        printf("%d %d\n", CsrColIndA_cuda[i], CsrColIndA_cpu[i]);
+    }
+    
+    
     // Perform matrix-matrix multiplication with the CSR-formatted matrix A
     start = clock();
     CHECK_CUSPARSE(cusparseScsrmm(handle, CUSPARSE_OPERATION_NON_TRANSPOSE, M,
@@ -190,11 +263,19 @@ int main(int argc, char **argv)
 
 
     printf("C:\n");
-    print_partial_matrix(C, M, M, 10, 10);
+    print_matrix(C, M, M);
 
     free(A);
     free(B);
     free(C);
+
+    free(CsrValA_cuda);
+    free(CsrRowPtrA_cuda);
+    free(CsrColIndA_cuda);
+
+    free(CsrValA_cpu);
+    free(CsrRowPtrA_cpu);
+    free(CsrColIndA_cpu);
 
     CHECK(cudaFree(dA));
     CHECK(cudaFree(dB));
